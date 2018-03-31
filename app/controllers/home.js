3652121f60e03cb0de6f1c5451b4ff15ca39ca83
Alasdair Monk <alasdair.monk@gmail.com>
2018-03-31 15:23:40 +0100

    latest

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,30 +1,42 @@
 import Controller from '@ember/controller';
 
-export default Controller.extend({
-  backgrounds: [
-    ""
-  ],
+export default Controller.extend ({
+  backgroundsAvailable: 13,
+  currentBg: 1,
 
-  randomQuote: Ember.computed('', {
+  backgroundColor: Ember.computed('currentBg', {
     get() {
-      let quotes = this.get('model');
-      return quotes[Math.floor(Math.random()*quotes.length)]
+      let bgs = this.get('backgroundsAvailable');
+      let currentBg = this.get('currentBg');
+      let nextBg = currentBg + 1;
+      if (nextBg > bgs) {
+        this.set('currentBg', 1)
+      }
+      return this.get('getBackgroundClassName');
+    }
+  }),
+
+  getBackgroundClassName: Ember.computed('currentBg', {
+    get() {
+      return `bg-${this.get('currentBg')}`; 
+    }
+  }),
+
+  randomQuote: Ember.computed ('', {
+    get () {
+      let quotes = this.get ('model');
+      return quotes[Math.floor (Math.random () * quotes.length)];
     },
-    set() {
-      let quotes = this.get('model');
-      return quotes[Math.floor(Math.random()*quotes.length)]
-    }
+    set () {
+      let quotes = this.get ('model');
+      return quotes[Math.floor (Math.random () * quotes.length)];
+    },
   }),
 
-  randomBackground: Ember.computed('', {
-    get() {
-      return "bg-1";
-    }
-  }),
-
-  actions : {
-    shuffleQuotes() {
-      this.set('randomQuote');
-    }
-  }
+  actions: {
+    shuffleQuotes () {
+      this.set ('randomQuote');
+      this.set ('currentBg', this.get('currentBg') + 1);
+    },
+  },
 });
---
+++ app/transitions.js
@@ -0,0 +1,6 @@
+export default function(){
+  this.transition(
+    this.childOf('#quote-text'),
+    this.use('toLeft', {duration: 200})
+  );
+}
